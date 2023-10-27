aae526fe30a4c93c20c7437cca3fc364285a6d25
Yorvin Luna Wisky <yorvin.luna@micm.gob.do>
2023-10-27 13:28:15 -0400

    Working on builds

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
+const morgan = require('morgan');
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
@@ -7,7 +9,14 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+app.use(morgan('combined'));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
 
 module.exports = app;
